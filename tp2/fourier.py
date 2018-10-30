e8a8432781356412480590b133729c3bbd9d4b51
matuteale <mbuscagl@itba.edu.ar>
2018-10-30 17:56:18 -0300

    Add magic code to make things happen

--- tp2/fourier.py
+++ tp2/fourier.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 
 from cmath import exp, pi
+import numpy as np
 
 def fft(x):
     N = len(x)
-    if N <= 1:
+    if N == 1:
         return x
-    even = fft(x[0::2])
-    odd = fft(x[1::2])
+    oddItems = fft(x[1::2])
+    evenItems = fft(x[0::2])
+    rangeToUse = range(N//2)
+    T = [exp(-2j * pi * k/N) * oddItems[k] for k in rangeToUse]
+    return np.concatenate([evenItems[k] + T[k] for k in rangeToUse], [evenItems[k] - T[k] for k in rangeToUse])
 
-    T = [exp(-2j * pi * k/N) * odd[k] for k in range(N//2)]
 
-    return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
+# def fft(x):
+#     N = len(x)
+#     if N <= 1:
+#         return x
+#     even = fft(x[0::2])
+#     odd = fft(x[1::2])
+
+#     T = [exp(-2j * pi * k/N) * odd[k] for k in range(N//2)]
+
+#     return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
